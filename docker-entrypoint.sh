60b6d2050205d2d65c0008cc7ac71dcd58196709
Anton Ovchinnikov <anton@tonyo.info>
2019-04-02 11:58:48 +0200

    feat: Local hostname for testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -eu
 
 if [ "$(id -u)" == "0" ]; then
+  echo '127.0.0.1 api.local.sentry.io' >> /etc/hosts  # For tests
   exec gosu symbolicator /bin/symbolicator "$@"
 else
   exec /bin/symbolicator "$@"
