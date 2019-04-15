b21dec1fabb6fa85f3a7d166d62db05f461ef97e
Jan Michael Auer <mail@jauer.org>
2019-04-15 09:23:10 +0200

    style: Fix clippy lints for Rust 1.34.0

--- src/logging.rs
+++ src/logging.rs
@@ -126,7 +126,7 @@ pub fn init_logging(config: &Config) {
 
 /// Returns whether backtrace printing is enabled.
 pub fn backtrace_enabled() -> bool {
-    match std::env::var("RUST_BACKTRACE").as_ref().map(|x| x.as_str()) {
+    match std::env::var("RUST_BACKTRACE").as_ref().map(String::as_str) {
         Ok("1") | Ok("full") => true,
         _ => false,
     }
