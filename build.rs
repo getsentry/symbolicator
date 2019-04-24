aa89682432ea88512aa5196797495703bb66d4cf
Armin Ronacher <armin.ronacher@active-4.com>
2019-04-24 21:22:00 +0200

    fix: Avoid pointless rebuilds (#40)

--- build.rs
+++ build.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::process::{Command, Stdio};
+
+fn emit_version_var() -> Result<(), io::Error> {
+    let cmd = Command::new("git")
+        .args(&["describe", "--always", "--dirty=-modified"])
+        .stderr(Stdio::inherit())
+        .output()?;
+
+    if !cmd.status.success() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("`git describe' failed: {}", cmd.status),
+        ));
+    }
+
+    let ver = String::from_utf8_lossy(&cmd.stdout);
+
+    println!("cargo:rustc-env=SYMBOLICATOR_GIT_VERSION={}", ver);
+    println!("cargo:rerun-if-changed=.git/index");
+
+    Ok(())
+}
+
 fn main() {
-    git_version::set_env_with_name("SYMBOLICATOR_GIT_VERSION");
+    emit_version_var().ok();
 }
