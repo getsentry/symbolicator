0cdb2a74396cb168f7fd23ba4e46c5a488c5bb71
Burak Yigit Kaya <byk@sentry.io>
2019-08-13 01:36:05 +0300

    feat(config): Default cache_dir to /data on Docker (#110)
    On our Docker images, we already define `/data` as a persistent volume, even more, it is quite tricky to mount a new cache directory as a volume with the right symbolicator user so make that the default on Docker. This also allows us to run symbolicator with the default config in getsentry/onpremise#220.

--- src/config.rs
+++ src/config.rs
@@ -190,10 +190,20 @@ fn default_bind() -> String {
     }
 }
 
+/// Default value for the "cache_dir" configuration.
+fn default_cache_dir() -> Option<PathBuf> {
+    if is_docker() {
+        // Docker image already defines `/data` as a persistent volume
+        Some(PathBuf::from("/data"))
+    } else {
+        None
+    }
+}
+
 impl Default for Config {
     fn default() -> Self {
         Config {
-            cache_dir: None,
+            cache_dir: default_cache_dir(),
             bind: default_bind(),
             logging: Logging::default(),
             metrics: Metrics::default(),
