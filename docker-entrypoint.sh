26e04c78b72316eab4506655ef6578f176f2fe0d
Burak Yigit Kaya <byk@sentry.io>
2019-08-14 13:56:05 +0300

    ref(docker): Various improvements on Docker image (#111)
    This patch does the following:
    
    - Upgrade to debian buster images
    - Move smoke tests to `cloudbuild.yaml`, reducing layers on prod image
    - Remove unnecessary `/etc/symbolicator` volume
    - Fix `/data` ownership change logic
    - Optimize build stage caching a bit
    - Fix the incorrect 'modified' suffix in version due to partial git copying

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -eu
 
 if [ "$(id -u)" == "0" ]; then
   # Prepare default data directory
-  mkdir -p /data
+  # WARNING(BYK): This should be done for the cache_dir mounted by any means, otherwise it is not
+  #               guaranteed to be writable, causing a runtime failure during downloads.
   chown symbolicator:symbolicator /data
 
   exec gosu symbolicator /bin/symbolicator "$@"
