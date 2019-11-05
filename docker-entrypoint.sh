096c3ebc11f0d9e3c84cdce233a849c89acd8cb2
Anton Ovchinnikov <anton@tonyo.info>
2019-11-05 13:44:17 +0100

    feat: Add an option to collect core dumps (#148)
    * feat: Add an option to collect core dumps
    
    * fix: Create the directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
+# Enable core dumps. Requires privileged mode.
+if [[ "$SYMBOLICATOR_DEBUG" == "1" ]]; then
+  mkdir -p /data/tmp
+  echo '/data/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+  ulimit -c unlimited
+fi
+
 if [ "$(id -u)" == "0" ]; then
   # Prepare default data directory
   # WARNING(BYK): This should be done for the cache_dir mounted by any means, otherwise it is not
