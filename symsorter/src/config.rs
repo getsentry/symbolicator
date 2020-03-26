e3aefb6f1aaa1309c8fb72a71ee997016da2bf2e
Armin Ronacher <armin.ronacher@active-4.com>
2020-03-26 13:16:59 +0100

    feat(symsorter): Add multi bundle support for easier sorting

---
+++ symsorter/src/config.rs
@@ -0,0 +1,48 @@
+use std::sync::{Arc, Mutex};
+use std::path::PathBuf;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CONFIG: Mutex<Arc<RunConfig>> = Mutex::new(Arc::new(Default::default()));
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Default, Clone)]
+pub struct RunConfig {
+    /// Output location for this task.
+    pub output: PathBuf,
+
+    /// Ignore broken archives.
+    pub ignore_errors: bool,
+
+    /// If enabled output will be suppressed
+    pub quiet: bool,
+}
+
+impl RunConfig {
+    pub fn get() -> Arc<RunConfig> {
+        CONFIG.lock().unwrap().clone()
+    }
+
+    pub fn configure<F: FnOnce(&mut Self) -> R, R>(f: F) -> R {
+        let mut config = RunConfig::get();
+        let rv = {
+            let mutable_config = Arc::make_mut(&mut config);
+            f(mutable_config)
+        };
+        *CONFIG.lock().unwrap() = config;
+        rv
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct SortConfig {
+    /// The bundle ID of this task.
+    pub bundle_id: String,
+
+    /// If enable the system will attempt to create source bundles
+    pub with_sources: bool,
+
+    /// If enabled debug symbols will be zstd compressed
+    /// (repeat to increase compression)
+    pub compression_level: usize,
+}
---
+++ symsorter/src/utils.rs
@@ -0,0 +1,88 @@
+use std::io::Cursor;
+use std::path::{Path, PathBuf};
+
+use failure::Error;
+use lazy_static::lazy_static;
+use regex::Regex;
+use symbolic::common::ByteView;
+use symbolic::debuginfo::sourcebundle::SourceBundleWriter;
+use symbolic::debuginfo::{Archive, FileFormat, Object, ObjectKind};
+
+lazy_static! {
+    static ref BAD_CHARS_RE: Regex = Regex::new(r"[^a-zA-Z0-9.,-]+").unwrap();
+}
+
+/// Makes a safe bundle ID from arbitrary input.
+pub fn make_bundle_id(input: &str) -> String {
+    BAD_CHARS_RE
+        .replace(input, "_")
+        .trim_matches(&['_'][..])
+        .to_string()
+}
+
+/// Checks if something is a safe bundle id
+pub fn is_bundle_id(input: &str) -> bool {
+    make_bundle_id(input) == input
+}
+
+/// Gets the unified ID from an object.
+pub fn get_unified_id(obj: &Object) -> String {
+    if obj.file_format() == FileFormat::Pe || obj.code_id().is_none() {
+        obj.debug_id().breakpad().to_string().to_lowercase()
+    } else {
+        obj.code_id().as_ref().unwrap().as_str().to_string()
+    }
+}
+
+/// Returns the intended target filename for an object.
+pub fn get_target_filename(obj: &Object) -> Option<PathBuf> {
+    let id = get_unified_id(obj);
+    // match the unified format here.
+    let suffix = match obj.kind() {
+        ObjectKind::Debug => "debuginfo",
+        ObjectKind::Sources => {
+            if obj.file_format() == FileFormat::SourceBundle {
+                "sourcebundle"
+            } else {
+                return None;
+            }
+        }
+        ObjectKind::Relocatable | ObjectKind::Library | ObjectKind::Executable => "executable",
+        _ => return None,
+    };
+    Some(format!("{}/{}/{}", &id[..2], &id[2..], suffix).into())
+}
+
+/// Creates a source bundle from a path.
+pub fn create_source_bundle(
+    path: &Path,
+    unified_id: &str,
+) -> Result<Option<ByteView<'static>>, Error> {
+    let bv = ByteView::open(path)?;
+    let archive = Archive::parse(&bv)?;
+    for obj in archive.objects() {
+        let obj = obj?;
+        if get_unified_id(&obj) == unified_id {
+            let mut out = Vec::<u8>::new();
+            let writer = SourceBundleWriter::start(Cursor::new(&mut out))?;
+            if writer.write_object(
+                &obj,
+                &path.file_name().unwrap().to_string_lossy().to_string(),
+            )? {
+                return Ok(Some(ByteView::from_vec(out)));
+            }
+        }
+    }
+    Ok(None)
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        {
+            if (!RunConfig::get().quiet) {
+                println!($($arg)*);
+            }
+        }
+    }
+}
