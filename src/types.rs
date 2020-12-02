103c88325eb1f8a39d0374719052adeb1ac54aec
Floris Bruynooghe <flub@sentry.io>
2020-12-02 15:13:21 +0100

    fix(procspawn): Ensure deserialisation
    If an object passed through procspawn can not correctly deserialise it
    will just hang forever, probably triggering the overal symbolication
    task timeout.

--- src/types.rs
+++ src/types.rs
@@ -443,7 +443,7 @@ pub struct CompleteObjectInfo {
     pub debug_status: ObjectFileStatus,
 
     /// Status for fetching the file with unwind info (for minidump stackwalking).
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", default)]
     pub unwind_status: Option<ObjectFileStatus>,
 
     /// Features available during symbolication.
