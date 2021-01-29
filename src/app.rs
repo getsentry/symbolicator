f4aa2bc34ca48fec09efd8e0071cb8ea3f5b0616
Jan Michael Auer <mail@jauer.org>
2021-01-29 12:31:26 +0100

    ref(endpoints): Remove redundant IO pools from functions

--- src/app.rs
+++ src/app.rs
@@ -16,10 +16,6 @@ use crate::utils::futures::ThreadPool;
 /// The shared state for the service.
 #[derive(Clone, Debug)]
 pub struct ServiceState {
-    /// Thread pool instance reserved for CPU-intensive tasks.
-    cpu_threadpool: ThreadPool,
-    /// Thread pool instance reserved for IO-intensive tasks.
-    io_threadpool: ThreadPool,
     /// Actor for minidump and stacktrace processing
     symbolication: SymbolicationActor,
     /// Actor for downloading and caching objects (no symcaches or cficaches)
@@ -27,18 +23,19 @@ pub struct ServiceState {
     /// The config object.
     config: Arc<Config>,
     /// The download service.
-    download_svc: Arc<DownloadService>,
+    downloader: Arc<DownloadService>,
 }
 
 impl ServiceState {
     pub fn create(config: Config) -> Result<Self> {
         let config = Arc::new(config);
 
-        let cpu_threadpool = ThreadPool::new();
-        let io_threadpool = ThreadPool::new();
+        let cpu_pool = ThreadPool::new();
+        let io_pool = ThreadPool::new();
+        let spawnpool = procspawn::Pool::new(config.processing_pool_size)
+            .context("failed to create process pool")?;
 
-        let download_svc = DownloadService::new(config.clone());
-
+        let downloader = DownloadService::new(config.clone());
         let caches = Caches::from_config(&config).context("failed to create local caches")?;
         caches
             .clear_tmp(&config)
@@ -46,37 +43,27 @@ impl ServiceState {
         let objects = ObjectsActor::new(
             caches.object_meta,
             caches.objects,
-            io_threadpool.clone(),
-            download_svc.clone(),
+            io_pool,
+            downloader.clone(),
         );
-        let symcaches =
-            SymCacheActor::new(caches.symcaches, objects.clone(), cpu_threadpool.clone());
-        let cficaches =
-            CfiCacheActor::new(caches.cficaches, objects.clone(), cpu_threadpool.clone());
-        let spawnpool = procspawn::Pool::new(config.processing_pool_size)
-            .context("failed to create process pool")?;
+        let symcaches = SymCacheActor::new(caches.symcaches, objects.clone(), cpu_pool.clone());
+        let cficaches = CfiCacheActor::new(caches.cficaches, objects.clone(), cpu_pool.clone());
 
         let symbolication = SymbolicationActor::new(
             objects.clone(),
             symcaches,
             cficaches,
             caches.diagnostics,
-            cpu_threadpool.clone(),
+            cpu_pool,
             spawnpool,
         );
 
         Ok(Self {
-            cpu_threadpool,
-            io_threadpool,
             symbolication,
             objects,
             config,
-            download_svc,
+            downloader,
         })
-    }
-
-    pub fn io_pool(&self) -> ThreadPool {
-        self.io_threadpool.clone()
     }
 
     pub fn symbolication(&self) -> SymbolicationActor {
