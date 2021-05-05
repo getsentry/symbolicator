3cac2d8f20c7d1ecb5e2b8eb6bf658663e7d9643
Arpad Borsos <arpad.borsos@sentry.io>
2021-05-05 15:20:26 +0200

    feat: Create a tool to feed minidumps and events to symbolicator

---
+++ crates/process-event/src/main.rs
@@ -0,0 +1,174 @@
+//! Tool to run Minidumps or Sentry Events through a local Symbolicator.
+
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+use std::path::PathBuf;
+
+use ::reqwest::blocking::multipart;
+use reqwest::blocking as reqwest;
+use structopt::StructOpt;
+
+/// Runs Minidumps or Sentry Events through Symbolicator.
+#[derive(Debug, StructOpt)]
+struct Cli {
+    /// Path to the input Minidump or Event JSON.
+    input: PathBuf,
+
+    /// The URL of the Symbolicator to use.
+    ///
+    /// Defaults to `http://127.0.0.1:3021` if not provided.
+    #[structopt(short, long)]
+    symbolicator: Option<String>,
+
+    /// Whether to include DIF candidate information.
+    #[structopt(short, long)]
+    dif_candidates: bool,
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let Cli {
+        input,
+        symbolicator,
+        dif_candidates,
+    } = Cli::from_args();
+
+    let client = reqwest::Client::new();
+    let symbolicator = symbolicator.as_deref().unwrap_or("http://127.0.0.1:3021");
+
+    let mut file = File::open(&input)?;
+
+    let mut magic = [0; 4];
+    file.read_exact(&mut magic)?;
+    file.seek(SeekFrom::Start(0))?;
+
+    let req = if &magic == b"MDMP" {
+        let req = client.post(&format!("{}/minidump", symbolicator));
+        let mut form = multipart::Form::new();
+
+        if dif_candidates {
+            form = form.text("options", r#"{"dif_candidates":true}"#);
+        }
+        form = form.file("upload_file_minidump", input)?;
+
+        req.multipart(form).send()
+    } else {
+        let req = client.post(&format!("{}/symbolicate", symbolicator));
+
+        let event = serde_json::from_reader(file)?;
+        let mut json = event::massage_event_json(event);
+        if dif_candidates {
+            if let Some(obj) = json.as_object_mut() {
+                obj.insert(
+                    String::from("options"),
+                    serde_json::json!({ "dif_candidates": true }),
+                );
+            }
+        }
+
+        req.json(&json).send()
+    };
+
+    req?.copy_to(&mut std::io::stdout())?;
+
+    Ok(())
+}
+
+mod event {
+    use serde::{Deserialize, Serialize};
+
+    /// Brings a Sentry JSON into the form suitable for Symbolicator.
+    ///
+    /// The `debug_meta.images` is renamed to `modules`, and it gathers all the `stacktrace`s of
+    /// exceptions and threads into the `stacktraces`. The stack traces are reversed, to match the
+    /// minidump output.
+    pub fn massage_event_json(event: Event) -> serde_json::Value {
+        let Event {
+            debug_meta,
+            exception,
+            threads,
+            ..
+        } = event;
+
+        let mut stacktraces = vec![];
+        if let Some(mut excs) = exception.map(|excs| excs.values) {
+            stacktraces.extend(excs.iter_mut().filter_map(|exc| exc.stacktrace.take()));
+        }
+        if let Some(mut threads) = threads.map(|threads| threads.values) {
+            stacktraces.extend(
+                threads
+                    .iter_mut()
+                    .filter_map(|thread| thread.stacktrace.take()),
+            );
+        }
+
+        for stacktrace in &mut stacktraces {
+            stacktrace.frames.reverse();
+        }
+
+        serde_json::json!({
+            "modules": debug_meta.images,
+            "stacktraces": stacktraces,
+        })
+    }
+
+    #[derive(Deserialize)]
+    pub struct Event {
+        debug_meta: DebugMeta,
+        exception: Option<Exceptions>,
+        threads: Option<Threads>,
+    }
+
+    #[derive(Deserialize)]
+    struct DebugMeta {
+        images: Vec<Image>,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    struct Image {
+        #[serde(rename = "type")]
+        ty: Option<String>,
+
+        image_addr: Option<String>,
+        image_size: Option<u64>,
+
+        code_id: Option<String>,
+        code_file: Option<String>,
+
+        debug_id: Option<String>,
+        debug_file: Option<String>,
+    }
+
+    #[derive(Deserialize)]
+    struct Exceptions {
+        values: Vec<Exception>,
+    }
+
+    #[derive(Deserialize)]
+    struct Exception {
+        stacktrace: Option<Stacktrace>,
+    }
+
+    #[derive(Deserialize)]
+    struct Threads {
+        values: Vec<Thread>,
+    }
+
+    #[derive(Deserialize)]
+    struct Thread {
+        stacktrace: Option<Stacktrace>,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    struct Stacktrace {
+        frames: Vec<Frame>,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    struct Frame {
+        function: Option<String>,
+        symbol: Option<String>,
+        package: Option<String>,
+        instruction_addr: Option<String>,
+        trust: Option<String>,
+    }
+}
