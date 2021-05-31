d89b89d8f53c38f4ff7d9f70451044e0177f3c7b
Frederic Branczyk <fbranczyk@gmail.com>
2021-05-31 13:57:33 +0200

    sources: Allow configuring a custom AWS region
    This can be useful when using an s3 compatible service, such as minio or
    Ceph.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 ### Features
 
+- S3 sources: add support for custom regions. ([#448](https://github.com/getsentry/symbolicator/pull/448))
 - Add support for BCSymbolMap auxiliary files which when present on the symbol server will automatically resolve obfuscated symbol names ([#403](https://github.com/getsentry/symbolicator/pull/403))
 - S3 sources: add support for using AWS container credentials provided by IAM task roles. ([#417](https://github.com/getsentry/symbolicator/pull/417))
 - Implement new Symbolication Quality Metrics. ([#426](https://github.com/getsentry/symbolicator/pull/426))
--- crates/symbolicator/src/sources.rs
+++ crates/symbolicator/src/sources.rs
@@ -1,12 +1,11 @@
 //! Download sources types and related implementations.
 
-use anyhow::Result;
 use std::collections::BTreeMap;
 use std::fmt;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use url::Url;
 
 use crate::types::{Glob, ObjectId, ObjectType};
@@ -123,20 +122,6 @@ pub struct FilesystemSourceConfig {
     pub files: CommonSourceConfig,
 }
 
-/// Local helper to deserializes an S3 region string in `S3SourceKey`.
-fn deserialize_region<'de, D>(deserializer: D) -> Result<rusoto_core::Region, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    // For safety reason we only want to parse the default AWS regions so that
-    // non AWS services cannot be accessed.
-    use serde::de::Error as _;
-    let region = String::deserialize(deserializer)?;
-    region
-        .parse()
-        .map_err(|e| D::Error::custom(format!("region: {}", e)))
-}
-
 /// The types of Amazon IAM credentials providers we support.
 ///
 /// For details on the AWS side, see:
@@ -158,7 +143,7 @@ impl Default for AwsCredentialsProvider {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct S3SourceKey {
     /// The region of the S3 bucket.
-    #[serde(deserialize_with = "deserialize_region")]
+    #[serde(default)]
     pub region: rusoto_core::Region,
 
     /// AWS IAM credentials provider for obtaining S3 access.
