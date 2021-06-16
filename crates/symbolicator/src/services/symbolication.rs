0f613fa1b1857eaeefc239add6deba48f710a34a
Sebastian Zivota <loewenheim@users.noreply.github.com>
2021-06-16 14:34:50 +0200

    Update crates/symbolicator/src/services/symbolication.rs
    Co-authored-by: Floris Bruynooghe <flub@sentry.io>

--- crates/symbolicator/src/services/symbolication.rs
+++ crates/symbolicator/src/services/symbolication.rs
@@ -18,7 +18,8 @@ use sentry::protocol::SessionStatus;
 use sentry::{Hub, SentryFutureExt};
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use similar::{udiff::unified_diff, Algorithm};
+use similar::Algorithm;
+use similar::udiff::unified_diff;
 use symbolic::common::{
     Arch, ByteView, CodeId, DebugId, InstructionInfo, Language, Name, SelfCell,
 };
