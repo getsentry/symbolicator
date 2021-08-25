21e44d60d99fcbe3eecddb190e94aef2fb8f7298
Arpad Borsos <arpad.borsos@sentry.io>
2021-08-25 17:24:33 +0200

    ref: Make sym/cficache versioning explicit (#533)
    This also allows loading outdated sym/cficaches, and provides instructions on how to bump the version, as well as static assertions that enforce version bumps whenever symbolics format version changes.

--- crates/symbolicator/src/macros.rs
+++ crates/symbolicator/src/macros.rs
@@ -11,3 +11,13 @@ macro_rules! compat_handler {
         }
     }};
 }
+
+/// Ensures at compile time that the condition is true.
+///
+/// See <https://github.com/rust-lang/rfcs/issues/2790>
+#[macro_export]
+macro_rules! static_assert {
+    ($condition:expr) => {
+        const _: &() = &[()][1 - ($condition) as usize];
+    };
+}
