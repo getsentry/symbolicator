3b8a151450175b21f5a170971db1d794a390b499
Sebastian Zivota <loewenheim@mailbox.org>
2021-08-26 10:28:54 +0200

    Revert "Convert tracing events to log records"
    This reverts commit 5142030efc4a2170e6f7382196ec322552f2008c.

--- crates/symbolicator/src/logging.rs
+++ crates/symbolicator/src/logging.rs
@@ -1,123 +1,34 @@
 use std::env;
 use std::fmt;
-use std::io::{self, Write};
 
-use chrono::{DateTime, Utc};
-use log::{Level, LevelFilter};
-use sentry::integrations::log::{breadcrumb_from_record, event_from_record};
-use serde::{Deserialize, Serialize};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 use crate::config::{Config, LogFormat};
 
 fn get_rust_log(level: LevelFilter) -> &'static str {
     match level {
-        LevelFilter::Off => "",
-        LevelFilter::Error => "ERROR",
-        LevelFilter::Warn => "WARN",
-        LevelFilter::Info => {
+        LevelFilter::OFF => "",
+        LevelFilter::ERROR => "ERROR",
+        LevelFilter::WARN => "WARN",
+        LevelFilter::INFO => {
             "INFO,\
              trust_dns_proto=WARN"
         }
-        LevelFilter::Debug => {
+        LevelFilter::DEBUG => {
             "INFO,\
              trust_dns_proto=WARN,\
              actix_web::pipeline=DEBUG,\
              symbolicator=DEBUG"
         }
-        LevelFilter::Trace => {
+        LevelFilter::TRACE => {
             "INFO,\
              trust_dns_proto=WARN,\
              actix_web::pipeline=DEBUG,\
              symbolicator=TRACE"
         }
-    }
-}
-
-fn pretty_logger() -> env_logger::Builder {
-    pretty_env_logger::formatted_builder()
-}
-
-fn simplified_logger() -> env_logger::Builder {
-    let mut builder = env_logger::Builder::new();
-    builder.format(|buf, record| {
-        writeln!(
-            buf,
-            "{} [{}] {}: {}",
-            buf.timestamp(),
-            record.module_path().unwrap_or("<unknown>"),
-            record.level(),
-            record.args()
-        )
-    });
-    builder
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct LogRecord<'a> {
-    timestamp: DateTime<Utc>,
-    level: Level,
-    logger: &'a str,
-    message: String,
-    module_path: Option<&'a str>,
-    filename: Option<&'a str>,
-    lineno: Option<u32>,
-}
-
-fn json_logger() -> env_logger::Builder {
-    let mut builder = env_logger::Builder::new();
-    builder.format(|mut buf, record| -> io::Result<()> {
-        let record = LogRecord {
-            timestamp: Utc::now(),
-            level: record.level(),
-            logger: record.target(),
-            message: record.args().to_string(),
-            module_path: record.module_path(),
-            filename: record.file(),
-            lineno: record.line(),
-        };
-
-        serde_json::to_writer(&mut buf, &record)
-            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-
-        buf.write_all(b"\n")?;
-        Ok(())
-    });
-    builder
-}
-
-/// A delegating logger that also logs breadcrumbs.
-pub struct BreadcrumbLogger<L> {
-    inner: L,
-}
-
-impl<L> BreadcrumbLogger<L> {
-    /// Initializes a new breadcrumb logger.
-    pub fn new(inner: L) -> Self {
-        Self { inner }
-    }
-}
-
-impl<L> log::Log for BreadcrumbLogger<L>
-where
-    L: log::Log,
-{
-    fn enabled(&self, md: &log::Metadata<'_>) -> bool {
-        self.inner.enabled(md)
-    }
-
-    fn log(&self, record: &log::Record<'_>) {
-        if self.inner.enabled(record.metadata()) {
-            if record.level() == log::Level::Error {
-                sentry::capture_event(event_from_record(record));
-            }
-
-            sentry::add_breadcrumb(|| breadcrumb_from_record(record));
-            self.inner.log(record);
-        }
-    }
-
-    fn flush(&self) {
-        self.inner.flush();
     }
 }
 
@@ -138,22 +49,26 @@ pub fn init_logging(config: &Config) {
         env::set_var("RUST_LOG", rust_log);
     }
 
-    let mut builder = match (config.logging.format, console::user_attended()) {
-        (LogFormat::Auto, true) | (LogFormat::Pretty, _) => pretty_logger(),
-        (LogFormat::Auto, false) | (LogFormat::Simplified, _) => simplified_logger(),
-        (LogFormat::Json, _) => json_logger(),
-    };
+    let filter = EnvFilter::from_default_env();
+    let sentry = sentry::integrations::tracing::layer();
+    let subscriber = FmtSubscriber::new().with(filter).with(sentry);
 
-    match env::var("RUST_LOG") {
-        Ok(rust_log) => builder.parse_filters(&rust_log),
-        Err(_) => builder.filter_level(config.logging.level),
-    };
+    let format = Layer::new().with_target(false);
+    match (config.logging.format, console::user_attended()) {
+        (LogFormat::Auto, true) | (LogFormat::Pretty, _) => {
+            tracing::subscriber::set_global_default(subscriber.with(format.pretty()))
+        }
+        (LogFormat::Auto, false) | (LogFormat::Simplified, _) => {
+            tracing::subscriber::set_global_default(subscriber.with(format.compact()))
+        }
+        (LogFormat::Json, _) => {
+            tracing::subscriber::set_global_default(subscriber.with(format.json()))
+        }
+    }
+    .expect("setting global default subscriber");
 
-    let logger = builder.build();
-    log::set_max_level(logger.filter());
-
-    let breadcrumb_logger = Box::new(BreadcrumbLogger::new(logger));
-    log::set_boxed_logger(breadcrumb_logger).unwrap();
+    // "Logger" that captures log records and republishes them as tracing events.
+    tracing_log::LogTracer::init().expect("setting global default log tracer");
 }
 
 /// A wrapper around an [`Error`](std::error::Error) that prints its causes.
@@ -174,8 +89,10 @@ impl<'a> fmt::Display for LogError<'a> {
 
 /// Logs an error to the configured logger or `stderr` if not yet configured.
 pub fn ensure_log_error(error: &anyhow::Error) {
-    if log::log_enabled!(log::Level::Error) {
-        log::error!("{:?}", error);
+    if tracing::Level::ERROR <= tracing::level_filters::STATIC_MAX_LEVEL
+        && tracing::Level::ERROR <= LevelFilter::current()
+    {
+        tracing::error!("{:?}", error);
     } else {
         eprintln!("{:?}", error);
     }
