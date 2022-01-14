3b37edb449dd1ab861dd2293975359deac5356f2
Floris Bruynooghe <flub@sentry.io>
2022-01-14 17:14:55 +0100

    fixup doc comment

--- crates/symbolicator/src/services/shared_cache.rs
+++ crates/symbolicator/src/services/shared_cache.rs
@@ -400,7 +400,7 @@ impl SharedCacheBackend {
     }
 }
 
-/// Message to send upload tasks across the [`SharedCacheService::upload_queue_tx`].
+/// Message to send upload tasks across the [`InnerSharedCacheService::upload_queue_tx`].
 struct UploadMessage {
     /// The cache key to store the data at.
     key: SharedCacheKey,
