c6ca153374d006546cc6b15afc32edcec30eadf7
Arpad Borsos <arpad.borsos@sentry.io>
2022-02-08 14:02:27 +0100

    meta: Add a wrk script to benchmark local symbolicator

---
+++ wrk_minidump.lua
@@ -0,0 +1,29 @@
+-- This is a `wrk` script that posts a multipart minidump request to symbolicator.
+-- It is best to prime the symbolicator caches by posting the minidump at least once prior to benchmarking:
+-- > cargo run -p process-event -- path/to/mini.dmp
+-- And then run this script, giving it the minidump file via env:
+-- > WRK_MINIDUMP="path/to/mini.dmp" wrk --threads 10 --connections 20 --duration 30s --script wrk_minidump.lua http://127.0.0.1:3021/minidump
+
+function read_file(path)
+  local file, errorMessage = io.open(path, "rb")
+  if not file then
+      error("Could not read the file:" .. errorMessage .. "\n")
+  end
+
+  local content = file:read "*all"
+  file:close()
+  return content
+end
+
+local Boundary = "----MinidumpUploadBoundary"
+local BodyBoundary = "--" .. Boundary
+local LastBoundary = "--" .. Boundary .. "--"
+local CRLF = "\r\n"
+local MinidumpFile = os.getenv("WRK_MINIDUMP")
+local FileBody = read_file(MinidumpFile)
+local ContentDisposition = 'Content-Disposition: form-data; name="upload_file_minidump"'
+
+wrk.method = "POST"
+wrk.path = "/minidump"
+wrk.headers["Content-Type"] = "multipart/form-data; boundary=" .. Boundary
+wrk.body = BodyBoundary .. CRLF .. ContentDisposition .. CRLF .. CRLF .. FileBody .. CRLF .. LastBoundary
