249c7fdc14e92fcd933b6832561803053e2a0203
Floris Bruynooghe <flub@sentry.io>
2022-03-09 15:15:21 +0100

    show me some things

--- crates/symbolicator/src/test.rs
+++ crates/symbolicator/src/test.rs
@@ -411,6 +411,7 @@ pub fn gcs_credentials_file() -> Result<Option<PathBuf>, std::io::Error> {
                 // Special case, see if we can create it from a env var, our CI sets this.
                 match std::env::var("GOOGLE_APPLICATION_CREDENTIALS_JSON") {
                     Ok(data) => {
+                        println!("HELLO YES, what is this?? {:?}", data);
                         std::fs::write(&path, data).unwrap();
                         path
                     }
@@ -446,10 +447,13 @@ macro_rules! gcs_credentials {
             },
             Ok(None) => {
                 match std::env::var("GOOGLE_APPLICATION_CREDENTIALS") {
-                    Ok(path) => $crate::test::TestGcsCredentials {
-                        bucket: "sentryio-symbolicator-cache-test".to_string(),
-                        credentials_file: Some(path.into()),
-                    },
+                    Ok(path) => {
+                        println!("WELL, seems like we have a credentials file??? {:?}", path);
+                        $crate::test::TestGcsCredentials {
+                            bucket: "sentryio-symbolicator-cache-test".to_string(),
+                            credentials_file: Some(path.into()),
+                        }
+                    }
                     Err(_) => {
                         println!("Skipping due to missing GOOGLE_APPLICATION_CREDENTIALS or gcs-service-account.json");
                         return;
