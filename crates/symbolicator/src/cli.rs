4ab5c947b05a9ba22f0457f28b86e78b1b10ed3a
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-11-02 09:41:39 +0100

    Set a more conservative profiles_sample_rate

--- crates/symbolicator/src/cli.rs
+++ crates/symbolicator/src/cli.rs
@@ -82,7 +82,7 @@ pub fn execute() -> Result<()> {
         // profiles are sub-sampling transactions, which we only sample based on upstream requests.
         // the sample rate we get from upstream is very low (< 20 transactions per minute, spread
         // across ~30 somewhat instances), so we might as well go full blast here.
-        profiles_sample_rate: 0.5,
+        profiles_sample_rate: 0.4,
         ..Default::default()
     });
 
