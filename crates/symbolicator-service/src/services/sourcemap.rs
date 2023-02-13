11e90dda3eec8456e0ca699b891b4dcea93b2253
Sebastian Zivota <loewenheim@mailbox.org>
2023-02-13 17:26:55 +0100

    address feedback

--- crates/symbolicator-service/src/services/sourcemap.rs
+++ crates/symbolicator-service/src/services/sourcemap.rs
@@ -65,19 +65,16 @@ impl SourceMapUrl {
     /// If the string starts with [`DATA_PREAMBLE`](Self::DATA_PREAMBLE), the rest is decoded from BASE64.
     /// Otherwise, the string is joined to the `base` URL.
     fn parse_with_prefix(base: &Url, url_string: &str) -> CacheEntry<Self> {
-        match url_string.strip_prefix(Self::DATA_PREAMBLE) {
-            Some(encoded) => {
-                let decoded = BASE64
-                    .decode(encoded.as_bytes())
-                    .map_err(|_| CacheError::Malformed("Invalid base64 sourcemap".to_string()))?;
-                Ok(Self::Data(decoded))
-            }
-            None => {
-                let url = base.join(url_string).map_err(|_| {
-                    CacheError::DownloadError("Invalid sourcemap url: {s}".to_string())
-                })?;
-                Ok(Self::Remote(url))
-            }
+        if let Some(encoded) = url_string.strip_prefix(Self::DATA_PREAMBLE) {
+            let decoded = BASE64
+                .decode(encoded.as_bytes())
+                .map_err(|_| CacheError::Malformed("Invalid base64 sourcemap".to_string()))?;
+            Ok(Self::Data(decoded))
+        } else {
+            let url = base.join(url_string).map_err(|_| {
+                CacheError::DownloadError(format!("Invalid sourcemap url: {url_string}"))
+            })?;
+            Ok(Self::Remote(url))
         }
     }
 }
