460fcac3876a12da56ffe3dcbb442b8e8f32e8a3
Sebastian Zivota <loewenheim@mailbox.org>
2023-02-15 15:35:56 +0100

    add a more complex test

--- crates/symbolicator-service/tests/integration/source_errors.rs
+++ crates/symbolicator-service/tests/integration/source_errors.rs
@@ -119,3 +119,149 @@ async fn test_download_errors() {
     // 7 requests, as the second requests should be served from cache
     assert_eq!(hitcounter.accesses(), 7);
 }
+
+#[tokio::test]
+async fn test_deny_list() {
+    let (symbolication, _cache_dir) = setup_service(|config| {
+        config.cache_dir = None;
+        config.max_download_timeout = Duration::from_millis(100);
+        config.deny_list_time_window = 1;
+        config.deny_list_bucket_size = 1;
+        config.deny_list_threshold = 2;
+        config.deny_list_block_time = Duration::from_secs(1);
+    });
+
+    let hitcounter = Server::new();
+
+    // This returns frame and module statuses:
+    // (
+    //     frame.status,
+    //     module.debug_status,
+    //     module.candidate.debug
+    //     module.candidate.download
+    // )
+    let get_statuses = |mut res: CompletedSymbolicationResponse| {
+        let frame = &res.stacktraces[0].frames[0];
+        let mut module = res.modules.pop().unwrap();
+        let candidate = module.candidates.0.pop().unwrap();
+        (
+            frame.status,
+            module.debug_status,
+            candidate.debug,
+            candidate.download,
+        )
+    };
+
+    let source = hitcounter.source("rejected", "/respond_statuscode/500/");
+    let request = example_request(vec![source]);
+
+    // The first two times should return 500
+    for _ in 0..2 {
+        let response = symbolication.symbolicate(request.clone()).await.unwrap();
+        assert_eq!(
+            get_statuses(response),
+            (
+                FrameStatus::Missing,
+                ObjectFileStatus::FetchingFailed,
+                ObjectUseInfo::None,
+                ObjectDownloadInfo::Error {
+                    details: "download failed: 500 Internal Server Error".into()
+                }
+            )
+        );
+    }
+
+    // For the third time the host shuold be blocked
+    let response = symbolication.symbolicate(request.clone()).await.unwrap();
+    assert_eq!(
+        get_statuses(response),
+        (
+            FrameStatus::Missing,
+            ObjectFileStatus::FetchingFailed,
+            ObjectUseInfo::None,
+            ObjectDownloadInfo::Error {
+                details: "download failed: Server is temporarily blocked".into()
+            }
+        )
+    );
+
+    let source = hitcounter.source("pending", "/delay/1h/");
+    let request = example_request(vec![source]);
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    // The first two times should return a timeout
+    for _ in 0..2 {
+        let response = symbolication.symbolicate(request.clone()).await.unwrap();
+        assert_eq!(
+            get_statuses(response),
+            (
+                FrameStatus::Missing,
+                ObjectFileStatus::Timeout,
+                ObjectUseInfo::None,
+                ObjectDownloadInfo::Error {
+                    details: "download timed out after 100ms".into()
+                }
+            )
+        );
+    }
+
+    // For the third time the host shuold be blocked
+    let response = symbolication.symbolicate(request.clone()).await.unwrap();
+    assert_eq!(
+        get_statuses(response),
+        (
+            FrameStatus::Missing,
+            ObjectFileStatus::FetchingFailed,
+            ObjectUseInfo::None,
+            ObjectDownloadInfo::Error {
+                details: "download failed: Server is temporarily blocked".into()
+            }
+        )
+    );
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    let source = hitcounter.source("notfound", "/respond_statuscode/404/");
+    let request = example_request(vec![source]);
+
+    // 404 should not result in blocking
+    for _ in 0..3 {
+        let response = symbolication.symbolicate(request.clone()).await.unwrap();
+        assert_eq!(
+            get_statuses(response),
+            (
+                FrameStatus::Missing,
+                ObjectFileStatus::Missing,
+                ObjectUseInfo::None,
+                ObjectDownloadInfo::NotFound
+            )
+        );
+    }
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    let source = hitcounter.source("permissiondenied", "/respond_statuscode/403/");
+    let request = example_request(vec![source]);
+
+    // 403 should not result in blocking
+    for _ in 0..3 {
+        let response = symbolication.symbolicate(request.clone()).await.unwrap();
+
+        assert_eq!(
+            get_statuses(response),
+            (
+                FrameStatus::Missing,
+                ObjectFileStatus::FetchingFailed,
+                ObjectUseInfo::None,
+                ObjectDownloadInfo::NoPerm {
+                    details: "403 Forbidden".into()
+                }
+            )
+        );
+    }
+
+    // server errors are tried up to 3 times, all others once, for a total of
+    // 14 requests
+    assert_eq!(hitcounter.accesses(), 14);
+}
