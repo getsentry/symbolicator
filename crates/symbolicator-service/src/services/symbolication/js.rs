419734d3793ac7e503fb4378f5cd8794f2f49bc2
Sebastian Zivota <loewenheim@mailbox.org>
2023-02-27 14:01:33 +0100

    Restore deleted comment

--- crates/symbolicator-service/src/services/symbolication/js.rs
+++ crates/symbolicator-service/src/services/symbolication/js.rs
@@ -42,6 +42,7 @@ impl SymbolicationActor {
                     Ok(key) => key,
                     Err(_) => {
                         symbolicated_frames.push(SymbolicatedJsFrame {
+                            // FIXME: this really means the `abs_path` is invalid!
                             status: JsFrameStatus::MissingSource,
                             raw: raw_frame.clone(),
                         });
