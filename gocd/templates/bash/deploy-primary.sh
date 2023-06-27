bf99924f8fc1b0bad1005adca08538a398c9d176
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-06-26 17:56:38 -0700

    Make ST and US deploys match up

--- gocd/templates/bash/deploy-primary.sh
+++ gocd/templates/bash/deploy-primary.sh
@@ -4,7 +4,7 @@ eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}"
 /devinfra/scripts/k8s/k8stunnel
 /devinfra/scripts/k8s/k8sdeploy.py \
   --type="statefulset" \
-  --label-selector="service=symbolicator,deploy_if_production=true" \
+  --label-selector="service=symbolicator" \
   --image="us.gcr.io/sentryio/symbolicator:${GO_REVISION_SYMBOLICATOR_REPO}" \
   --container-name="symbolicator" \
   --container-name="cleanup"
