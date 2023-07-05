6259453df74680583ef6870bd0e5ea9907421e04
Sebastian Zivota <loewenheim@mailbox.org>
2023-07-05 13:31:50 +0200

    don't return paths multiple times from SourceLocationIter

--- crates/symbolicator-sources/src/remotefile.rs
+++ crates/symbolicator-sources/src/remotefile.rs
@@ -3,8 +3,8 @@
 //! This provides the [`RemoteFile`] type which provides a unified way of dealing with a file
 //! which may exist on a source.
 
-use std::fmt;
 use std::path::Path;
+use std::{collections::HashSet, fmt};
 
 use serde::{Deserialize, Serialize};
 use url::Url;
@@ -95,6 +95,9 @@ pub struct SourceLocationIter<'a> {
 
     /// Remaining locations to iterate.
     next: Vec<String>,
+
+    /// Paths that were previously returned.
+    previously_returned: HashSet<String>,
 }
 
 impl<'a> SourceLocationIter<'a> {
@@ -110,6 +113,7 @@ impl<'a> SourceLocationIter<'a> {
             object_id,
             layout: config.layout,
             next: vec![],
+            previously_returned: HashSet::new(),
         }
     }
 }
@@ -129,7 +133,14 @@ impl Iterator for SourceLocationIter<'_> {
             }
         }
 
-        self.next.pop().map(SourceLocation::new)
+        self.next.pop().and_then(|next| {
+            if !self.previously_returned.contains(&next) {
+                self.previously_returned.insert(next.clone());
+                Some(SourceLocation::new(next))
+            } else {
+                None
+            }
+        })
     }
 }
 
