c1b23d24d30ce6af17788016e27c298a8dc12c4d
Joshua Li <joshuarli98@gmail.com>
2023-07-17 14:02:43 -0700

    validate pipelines in CI

--- .github/workflows/lint-pipelines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# gocd-cli does not catch all errors, but does catch some simple issues.
-# A better solution may be: https://github.com/GaneshSPatil/gocd-mergeable
-
-echo "GoCD YAML Linting"
-
-find "gocd" -name "*.yaml" -type f -print0 | \
-  xargs -0 -I'{}' bash -c 'printf  "\n🔎 Linting {}\n\t" && gocd-cli configrepo syntax --yaml --raw "{}"'
