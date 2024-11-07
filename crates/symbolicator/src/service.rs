3d241153e8bbd06bb6bb689f597bdb76c1b7c44e
Sebastian Zivota <loewenheim@users.noreply.github.com>
2024-11-07 11:20:29 +0100

    ref(metrics): Tag requests accepted/processed with task name (#1548)

--- crates/symbolicator/src/service.rs
+++ crates/symbolicator/src/service.rs
@@ -404,7 +404,9 @@ impl RequestService {
             }
         }
 
-        metric!(counter("requests.accepted") += 1);
+        // Using `task_name` as the tag should be fine, there is only a small
+        // fixed number of them.
+        metric!(counter("requests.accepted") += 1, "task" => task_name);
 
         let request_id = RequestId::new(uuid::Uuid::new_v4());
         requests
@@ -479,7 +481,10 @@ impl RequestService {
             // We stop counting the request as an in-flight request at this point, even though
             // it will stay in the `requests` map for another 90s.
             current_requests.fetch_sub(1, Ordering::Relaxed);
-            metric!(counter("requests.processed") += 1);
+
+            // Using `task_name` as the tag should be fine, there is only a small
+            // fixed number of them.
+            metric!(counter("requests.processed") += 1, "task" => task_name);
 
             // Wait before removing the channel from the computation map to allow clients to
             // poll the status.
