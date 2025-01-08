890bb19d97be7827cc5358c117907ca72a06c309
Sebastian Zivota <loewenheim@mailbox.org>
2025-01-08 16:52:31 +0100

    Failing test

--- crates/symbolicator-service/src/caching/tests.rs
+++ crates/symbolicator-service/src/caching/tests.rs
@@ -1,5 +1,5 @@
 use std::fs::{self, File};
-use std::io::Write;
+use std::io::{self, Write};
 use std::path::Path;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread::sleep;
@@ -916,4 +916,62 @@ async fn test_lazy_computation_limit() {
     }
 
     assert_eq!(num_outdated, 2);
+}
+
+/// A request to compute a cache item that always fails.
+#[derive(Clone, Default)]
+struct FailingTestCacheItem;
+
+impl CacheItemRequest for FailingTestCacheItem {
+    type Item = String;
+
+    const VERSIONS: CacheVersions = CacheVersions {
+        current: 1,
+        fallbacks: &[],
+    };
+
+    fn compute<'a>(&'a self, temp_file: &'a mut NamedTempFile) -> BoxFuture<'a, CacheEntry> {
+        Box::pin(async move {
+            fs::write(temp_file.path(), "garbage data")?;
+            Err(io::Error::from(io::ErrorKind::WriteZero))?
+        })
+    }
+
+    fn load(&self, data: ByteView<'static>) -> CacheEntry<Self::Item> {
+        Ok(std::str::from_utf8(data.as_slice()).unwrap().to_owned())
+    }
+}
+
+/// Verifies that an io error during computation results in the temporary
+/// file being discarded instead of persisted.
+#[tokio::test]
+async fn test_failing_cache_write() {
+    test::setup();
+    let cache_dir = test::tempdir();
+
+    let request = FailingTestCacheItem;
+    let key = CacheKey::for_testing("global/some_cache_key");
+
+    let config = Config {
+        cache_dir: Some(cache_dir.path().to_path_buf()),
+        ..Default::default()
+    };
+    let cache = Cache::from_config(
+        CacheName::Objects,
+        &config,
+        CacheConfig::from(CacheConfigs::default().derived),
+        Arc::new(AtomicIsize::new(1)),
+        1024,
+    )
+    .unwrap();
+    let cacher = Cacher::new(cache, Default::default());
+
+    // An `io::Error` during computation should be converted to an `InternalError`
+    let entry = cacher.compute_memoized(request, key.clone()).await;
+    assert!(matches!(entry, Err(CacheError::InternalError)));
+
+    // The computation returned an `io::Error`, so the file should not have been
+    // persisted
+    let cache_file_path = cache_dir.path().join("objects").join(key.cache_path(1));
+    assert!(!fs::exists(cache_file_path).unwrap());
 }
