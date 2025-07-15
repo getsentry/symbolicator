79879462f3da2e2595d63a0f2bb00fe8cee47684
mchen-sentry <ming.chen@sentry.io>
2025-07-15 09:48:32 -0700

    ref(gocd): use console script entry points (#1729)

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")
-/devinfra/scripts/k8s/k8stunnel
-/devinfra/scripts/k8s/k8s-deploy.py \
+eval $(regions-project-env-vars --region="${SENTRY_REGION}")
+/devinfra/scripts/get-cluster-credentials
+k8s-deploy \
   --type="statefulset" \
   --label-selector="${LABEL_SELECTOR}" \
   --image="us-central1-docker.pkg.dev/sentryio/symbolicator/image:${GO_REVISION_SYMBOLICATOR_REPO}" \
