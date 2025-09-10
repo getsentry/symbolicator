d068da7be7c14a96a675c4e42de9474b84ae5a72
Sebastian Zivota <loewenheim@mailbox.org>
2025-09-10 16:06:01 +0200

    Revert "Fix test"
    This reverts commit 4c9769f9d2b2895f747693df307e692e6b27a29b.

--- crates/symbolicator-native/tests/integration/e2e.rs
+++ crates/symbolicator-native/tests/integration/e2e.rs
@@ -235,9 +235,7 @@ async fn test_no_permission() {
     );
     assert_eq!(
         candidates[5].download,
-        ObjectDownloadInfo::Error {
-            details: "download failed: unhandled error".to_owned()
-        }
+        ObjectDownloadInfo::NoPerm { details: "The authorization header is malformed; a non-empty Access Key (AKID) must be provided in the credential.".into() }
     );
 }
 
