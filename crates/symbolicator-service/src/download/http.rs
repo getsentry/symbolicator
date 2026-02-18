2b995745fdb2d106a3d086a837b4490273a6b820
tobias-wilfert <tobias.wilfert@sentry.io>
2026-02-18 11:39:20 +0100

    do not double write `user-agent`

--- crates/symbolicator-service/src/download/http.rs
+++ crates/symbolicator-service/src/download/http.rs
@@ -46,6 +46,19 @@ impl HttpDownloader {
             self.client.get(download_url)
         };
 
+        // Only set `symbolicator` as the user agent if the `file_source` does not have a custom
+        // user agent that should be used.
+        let has_custom_user_agent = file_source
+            .source
+            .headers
+            .0
+            .keys()
+            .any(|k| k.eq_ignore_ascii_case("user-agent"));
+
+        if !has_custom_user_agent {
+            builder = builder.header(header::USER_AGENT, USER_AGENT);
+        }
+
         builder = builder.header(header::USER_AGENT, USER_AGENT);
         let headers = file_source
             .source
