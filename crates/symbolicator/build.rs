1dca7e0d3c2fbce3e291867ee01bcb60afef9861
Joris Bayer <joris.bayer@sentry.io>
2026-03-17 10:43:37 +0100

    build: Fail if env vars cannot be set (#1906)

--- crates/symbolicator/build.rs
+++ crates/symbolicator/build.rs
@@ -1,49 +1,40 @@
-use std::io;
 use std::process::{Command, Stdio};
 
-fn emit_version_var() -> Result<(), io::Error> {
+fn emit_version_var() {
     let cmd = Command::new("git")
         .args(["describe", "--always", "--dirty=-modified"])
         .stderr(Stdio::inherit())
-        .output()?;
+        .output()
+        .expect("read from stderr");
 
     if !cmd.status.success() {
-        return Err(io::Error::other(format!(
-            "`git describe' failed: {}",
-            cmd.status
-        )));
+        panic!("`git describe' failed: {}", cmd.status);
     }
 
     let ver = String::from_utf8_lossy(&cmd.stdout);
 
     println!("cargo:rustc-env=SYMBOLICATOR_GIT_VERSION={ver}");
     println!("cargo:rerun-if-env-changed=SYMBOLICATOR_GIT_VERSION");
-
-    Ok(())
 }
 
-fn emit_release_var() -> Result<(), io::Error> {
+fn emit_release_var() {
     let cmd = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .stderr(Stdio::inherit())
-        .output()?;
+        .output()
+        .expect("read from stderr");
 
     if !cmd.status.success() {
-        return Err(io::Error::other(format!(
-            "`git rev-parse' failed: {}",
-            cmd.status
-        )));
+        panic!("`git rev-parse' failed: {}", cmd.status);
     }
 
     let ver = String::from_utf8_lossy(&cmd.stdout);
 
     println!("cargo:rustc-env=SYMBOLICATOR_RELEASE={ver}");
     println!("cargo:rerun-if-env-changed=SYMBOLICATOR_RELEASE");
-
-    Ok(())
 }
 
 fn main() {
-    emit_version_var().ok();
-    emit_release_var().ok();
+    emit_version_var();
+    emit_release_var();
 }
