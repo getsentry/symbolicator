7bc7939a8d202db0009c3563fac3d0128b56180d
Joris Bayer <joris.bayer@sentry.io>
2026-03-17 08:11:56 +0100

    ref: reuse layer

--- crates/symbolicator-service/src/logging.rs
+++ crates/symbolicator-service/src/logging.rs
@@ -1,21 +1,25 @@
-use sentry::integrations::tracing::EventFilter;
+use sentry::integrations::tracing::{EventFilter, SentryLayer};
 use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::fmt::{MakeWriter, fmt};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::util::SubscriberInitExt;
 
+/// Same as the default filter, except it sends everything at or above INFO as logs instead of breadcrumbs.
+pub fn sentry_layer<S>() -> SentryLayer<S>
+where
+    S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>,
+{
+    sentry::integrations::tracing::layer().event_filter(|md| match *md.level() {
+        tracing::Level::ERROR | tracing::Level::WARN => EventFilter::Event | EventFilter::Log,
+        tracing::Level::INFO => EventFilter::Log,
+        tracing::Level::DEBUG | tracing::Level::TRACE => EventFilter::Ignore,
+    })
+}
+
 pub fn init_json_logging<W>(env_filter: &str, make_writer: W)
 where
     W: for<'writer> MakeWriter<'writer> + Send + Sync + 'static,
 {
-    // Same as the default filter, except it sends everything at or above INFO as logs instead of breadcrumbs.
-    let sentry_layer =
-        sentry::integrations::tracing::layer().event_filter(|md| match *md.level() {
-            tracing::Level::ERROR => EventFilter::Event | EventFilter::Log,
-            tracing::Level::WARN | tracing::Level::INFO => EventFilter::Log,
-            tracing::Level::DEBUG | tracing::Level::TRACE => EventFilter::Ignore,
-        });
-
     fmt()
         .with_timer(UtcTime::rfc_3339())
         .with_target(true)
@@ -28,6 +32,6 @@ where
         .with_line_number(true)
         .with_writer(make_writer)
         .finish()
-        .with(sentry_layer)
+        .with(sentry_layer())
         .init();
 }
