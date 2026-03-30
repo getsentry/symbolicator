e368d488f296ba7ffff60cfbdc6e9aa8414c53fb
Sebastian Zivota <loewenheim@mailbox.org>
2026-03-30 10:47:26 +0200

    build: Remove `git describe` call from build.rs
    This command sometimes causes problems in GitHub CI. As a side effect of
    this change, the "git commit" printed by `symbolicator --version` changes
    from a tag + commit to a bare commit hash.

--- crates/symbolicator/build.rs
+++ crates/symbolicator/build.rs
@@ -1,22 +1,5 @@
 use std::process::{Command, Stdio};
 
-fn emit_version_var() {
-    let cmd = Command::new("git")
-        .args(["describe", "--always", "--dirty=-modified"])
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("read from stderr");
-
-    if !cmd.status.success() {
-        panic!("`git describe' failed: {}", cmd.status);
-    }
-
-    let ver = String::from_utf8_lossy(&cmd.stdout);
-
-    println!("cargo:rustc-env=SYMBOLICATOR_GIT_VERSION={ver}");
-    println!("cargo:rerun-if-env-changed=SYMBOLICATOR_GIT_VERSION");
-}
-
 fn emit_release_var() {
     let cmd = Command::new("git")
         .args(["rev-parse", "HEAD"])
@@ -35,6 +18,5 @@ fn emit_release_var() {
 }
 
 fn main() {
-    emit_version_var();
     emit_release_var();
 }
