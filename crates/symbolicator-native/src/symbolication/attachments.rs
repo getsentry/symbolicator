737066ed07463cfcb6eedb8fc7ace886fb2cf9f9
David Herberth <david.herberth@sentry.io>
2026-05-06 09:29:21 +0200

    use a cache error

--- crates/symbolicator-native/src/symbolication/attachments.rs
+++ crates/symbolicator-native/src/symbolication/attachments.rs
@@ -1,17 +1,16 @@
 use std::fs::File;
-use std::pin::pin;
 
 use futures::TryStreamExt;
+use symbolicator_service::caching::CacheError;
 use symbolicator_service::download::DownloadService;
 use tokio::io::{AsyncSeekExt, AsyncWriteExt, BufWriter};
-use tokio_util::io::StreamReader;
 
 use crate::interface::AttachmentFile;
 
 pub async fn download_attachment(
     download_svc: &DownloadService,
     file: AttachmentFile,
-) -> anyhow::Result<File> {
+) -> Result<File, CacheError> {
     let (storage_url, storage_token) = match file {
         AttachmentFile::Local(file) => return Ok(file),
         AttachmentFile::Remote {
@@ -29,17 +28,13 @@ pub async fn download_attachment(
     if let Some(token) = storage_token {
         request = request.bearer_auth(token);
     }
-    let stream = request
-        .send()
-        .await?
-        .error_for_status()?
-        .bytes_stream()
-        .map_err(std::io::Error::other);
-    let mut reader = pin!(StreamReader::new(stream));
+    let mut stream = request.send().await?.error_for_status()?.bytes_stream();
 
     let file = tempfile::tempfile()?;
     let mut writer = BufWriter::new(tokio::fs::File::from_std(file));
-    tokio::io::copy(&mut reader, &mut writer).await?;
+    while let Some(chunk) = stream.try_next().await? {
+        writer.write_all(&chunk).await?;
+    }
     writer.flush().await?;
     let mut file = writer.into_inner();
     file.sync_data().await?;
