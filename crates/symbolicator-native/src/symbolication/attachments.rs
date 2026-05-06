c267f1fddfd8fd84216ec5c447ee4cd93fa7ae9e
David Herberth <david.herberth@sentry.io>
2026-05-06 09:31:57 +0200

    use retry

--- crates/symbolicator-native/src/symbolication/attachments.rs
+++ crates/symbolicator-native/src/symbolication/attachments.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 
 use futures::TryStreamExt;
 use symbolicator_service::caching::CacheError;
-use symbolicator_service::download::DownloadService;
+use symbolicator_service::download::{self, DownloadService};
 use tokio::io::{AsyncSeekExt, AsyncWriteExt, BufWriter};
 
 use crate::interface::AttachmentFile;
@@ -19,27 +19,30 @@ pub async fn download_attachment(
         } => (storage_url, storage_token),
     };
 
-    // TODO: maybe its worth using the actual `DownloadService` instead of straight going to the `trusted_client`.
-    // Doing so would in theory allow us to have retries and error report, as well as being able to
-    // download files in multiple chunks concurrently, but I don’t think our `objecstore` server currently
-    // supports range requests, and those would also mess with streaming decompression.
-    // Not to mention that using the `DownloadService` is not that straight forward.
-    let mut request = download_svc.trusted_client.get(storage_url);
-    if let Some(token) = storage_token {
-        request = request.bearer_auth(token);
-    }
-    let mut stream = request.send().await?.error_for_status()?.bytes_stream();
+    download::retry(|| async {
+        // TODO: maybe its worth using the actual `DownloadService` instead of straight going to the `trusted_client`.
+        // Doing so would in theory allow us to have retries and error report, as well as being able to
+        // download files in multiple chunks concurrently, but I don’t think our `objecstore` server currently
+        // supports range requests, and those would also mess with streaming decompression.
+        // Not to mention that using the `DownloadService` is not that straight forward.
+        let mut request = download_svc.trusted_client.get(&storage_url);
+        if let Some(token) = storage_token.as_ref() {
+            request = request.bearer_auth(token);
+        }
+        let mut stream = request.send().await?.error_for_status()?.bytes_stream();
 
-    let file = tempfile::tempfile()?;
-    let mut writer = BufWriter::new(tokio::fs::File::from_std(file));
-    while let Some(chunk) = stream.try_next().await? {
-        writer.write_all(&chunk).await?;
-    }
-    writer.flush().await?;
-    let mut file = writer.into_inner();
-    file.sync_data().await?;
+        let file = tempfile::tempfile()?;
+        let mut writer = BufWriter::new(tokio::fs::File::from_std(file));
+        while let Some(chunk) = stream.try_next().await? {
+            writer.write_all(&chunk).await?;
+        }
+        writer.flush().await?;
+        let mut file = writer.into_inner();
+        file.sync_data().await?;
 
-    file.rewind().await?;
+        file.rewind().await?;
 
-    Ok(file.into_std().await)
+        Ok(file.into_std().await)
+    })
+    .await
 }
