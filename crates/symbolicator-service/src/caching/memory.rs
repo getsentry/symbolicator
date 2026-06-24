e6763bef9fc4ade52c1e6dfd23df360a67064882
David Herberth <david.herberth@sentry.io>
2026-06-24 10:14:41 +0200

    fix(api): Reduce inflight requests on panic

--- crates/symbolicator-service/src/caching/memory.rs
+++ crates/symbolicator-service/src/caching/memory.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
-use std::sync::atomic::Ordering;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
@@ -549,20 +548,16 @@ impl<T: CacheItemRequest> Cacher<T> {
             return;
         }
 
-        // We count down towards zero, and if we reach or surpass it, we will stop here.
-        let max_lazy_refreshes = self.config.max_lazy_refreshes();
-        if max_lazy_refreshes.fetch_sub(1, Ordering::Relaxed) <= 0 {
-            max_lazy_refreshes.fetch_add(1, Ordering::Relaxed);
-
+        let Some(lazy_refresh_token) = self.config.lazy_refresh().try_incr() else {
             metric!(counter("caches.lazy_limit_hit") += 1, "cache" => name.as_str());
             return;
-        }
+        };
 
         let done_token = {
             let key = cache_key.clone();
             let refreshes = Arc::clone(&self.refreshes);
             defer(move || {
-                max_lazy_refreshes.fetch_add(1, Ordering::Relaxed);
+                drop(lazy_refresh_token);
                 refreshes.lock().unwrap().remove(&key);
             })
         };
@@ -578,7 +573,7 @@ impl<T: CacheItemRequest> Cacher<T> {
 
         let this = self.clone();
         let task = async move {
-            let _done_token = done_token; // move into the future
+            let _done_token = done_token;
 
             let span = sentry::configure_scope(|scope| scope.get_span());
             let ctx = sentry::TransactionContext::continue_from_span(
