bab923afad3d126509cc28b5d65bfdfe29bf17f9
David Herberth <david.herberth@sentry.io>
2026-08-28 09:49:57 +0200

    ref(native): Make demangle cache as a wrapper type (#2018)
    Small refactor to make the cache easier to use and to have a central
    place for demangling. For example only 1 of 2 code paths actually used
    the cache for demangling.

--- crates/symbolicator-native/src/symbolication/native.rs
+++ crates/symbolicator-native/src/symbolication/native.rs
@@ -9,7 +9,7 @@ use crate::interface::{
     AdjustInstructionAddr, FrameStatus, RawFrame, Registers, Signal, SymbolicatedFrame,
 };
 
-use super::demangle::{DemangleCache, demangle_symbol};
+use super::demangle::DemangleCache;
 use super::module_lookup::CacheLookupResult;
 
 pub fn symbolicate_native_frame(
@@ -38,7 +38,9 @@ pub fn symbolicate_native_frame(
         let filename = split_path(&abs_path).1;
 
         let func = source_location.function();
-        let (symbol, function) = demangle_symbol(demangle_cache, &func);
+        let function = demangle_cache
+            .demangle_function(&func)
+            .unwrap_or_else(|| func.name().to_owned());
 
         sym_addr = Some(HexValue(
             lookup_result.expose_preferred_addr(func.entry_pc() as u64),
@@ -64,7 +66,7 @@ pub fn symbolicate_native_frame(
                 instruction_addr,
                 adjust_instruction_addr: frame.adjust_instruction_addr,
                 function_id: frame.function_id,
-                symbol: Some(symbol),
+                symbol: Some(func.name().to_owned()),
                 abs_path: if !abs_path.is_empty() {
                     Some(abs_path)
                 } else {
